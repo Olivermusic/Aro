0f734f0be5a55a318b644f51aec3c8aa13a7e31e
Olivermusic <121124279+Olivermusic@users.noreply.github.com>
2024-06-04 13:03:33 +0300

    Update نادي المطور.py

--- AarohiX/plugins/play/نادي المطور.py
+++ AarohiX/plugins/play/نادي المطور.py
@@ -38,8 +38,8 @@ async def call_dev(client: Client, message: Message):
                                      reply_markup=reply_markup)
 
     # إنشاء زر "اونلاين"
-    online_button = InlineKeyboardButton("< freedom >", url=f"https://t.me/{Muntazer}")
+    online_button = InlineKeyboardButton("< Oliver >", url=f"https://t.me/LLL7P")
     
-    await message.reply_text(f"~ **تم إرسال النداء إلى مطور البوت\n\n-› Master -› @{Muntazer} .",
+    await message.reply_text(f"~ **تم إرسال النداء إلى مطور البوت\n\n-› Master -› @LLL7P .",
                              disable_web_page_preview=True,
                              reply_markup=InlineKeyboardMarkup([[online_button]]))
