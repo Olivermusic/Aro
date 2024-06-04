544d06742cff88f071aeff0400f01e533a55aece
Olivermusic <121124279+Olivermusic@users.noreply.github.com>
2024-06-04 21:36:34 +0300

    Update queue.py

--- AarohiX/utils/inline/queue.py
+++ AarohiX/utils/inline/queue.py
@@ -28,7 +28,7 @@ def queue_markup(
                 text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="「 بواسطة 」", url=SUPPORT_CHAT,
+                text="「 𓏺 𝘢 𝘍𝘪𝘶𝘯𝘺  」", url=SUPPORT_CHAT,
             ),
         ],
     ]
@@ -54,7 +54,7 @@ def queue_markup(
                 text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="「 بواسطة 」", url=SUPPORT_CHAT,
+                text="「 𓏺 𝘢 𝘍𝘪𝘶𝘯𝘺  」", url=SUPPORT_CHAT,
             ),
         ],
     ]
@@ -80,7 +80,7 @@ def queue_back_markup(_, CPLAY):
                 text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="「 بواسطة 」", url=SUPPORT_CHAT,
+                text="「 𓏺 𝘢 𝘍𝘪𝘶𝘯𝘺  」", url=SUPPORT_CHAT,
             ),
         ],
         ]
@@ -101,7 +101,7 @@ def aq_markup(_, chat_id):
                 text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="「 بواسطة 」", url=SUPPORT_CHAT,
+                text="「 𓏺 𝘢 𝘍𝘪𝘶𝘯𝘺  」", url=SUPPORT_CHAT,
             ),
         ],
     ]
